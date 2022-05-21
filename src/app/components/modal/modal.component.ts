a294f04cde890d0e5c21fb60a66f00151a997025
GeorgeOrzanescu <george.asro@gmail.com>
2022-05-21 18:55:27 +0300

    Added all structure
    Signed-off-by: GeorgeOrzanescu <george.asro@gmail.com>

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,10 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
   @Input() isModalOpen: boolean;
-
+  @Output() toggleModal: EventEmitter<boolean> = new EventEmitter<boolean>();
   constructor() {
     this.isModalOpen = false;
   }
 
   ngOnInit(): void {}
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.toggleModal.emit(this.isModalOpen);
+  }
 }
---
+++ src/app/components/read-more-btn/read-more-btn.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReadMoreBtnComponent } from './read-more-btn.component';
+
+describe('ReadMoreBtnComponent', () => {
+  let component: ReadMoreBtnComponent;
+  let fixture: ComponentFixture<ReadMoreBtnComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReadMoreBtnComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReadMoreBtnComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
